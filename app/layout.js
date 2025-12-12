71a1e4b0590d7920d2ca9ded69355cb5d7b654d9
Aftab Ahmed <58620536+aftab23822@users.noreply.github.com>
2025-12-12 16:47:09 +0500

    adsense verfication code

--- app/layout.js
+++ app/layout.js
@@ -93,6 +93,12 @@ export default function RootLayout({ children }) {
             })
           }}
         />
+        
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5652591674967437"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className={inter.className}>
         {children}
